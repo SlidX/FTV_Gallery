03ff4e6b221eda126f79a7b2c05243c0a05862b8
Pedro <slidxweb@yandex.ru>
2020-03-28 16:23:07 +0300

    add hash

--- README.md
+++
@@ -1,2 +0,0 @@
-# FTV_Gallery
-It's our project, which involves a portfolio of work. F()__0
---
+++ src/scrypt/index_main.js
@@ -0,0 +1,3 @@
+document.querySelector(".scrollTo__ball").addEventListener('click', () => { 
+    window.scrollTo(0,0)
+})
